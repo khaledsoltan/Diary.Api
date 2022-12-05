5dbfeecbeb9f52a05cdeb227d7987e33bdb87cfe
khaled soltan <khaled.sultan.2022@hotmail.com>
2022-12-05 19:19:47 +0200

    Build Layers

---
+++ Api.Host/Extensions/ServiceExtensions.cs
@@ -0,0 +1,215 @@
+﻿using AspNetCoreRateLimit;
+using Business.ServiceLocator.Contracts;
+using Business.ServiceLocator.Implmentation;
+using Domain.Auth;
+using Domain.ConfigurationModels;
+using Marvin.Cache.Headers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Presentation.Controllers;
+using Repository.Context;
+using Repository.UnitOfWork;
+using Shared.LoggerService;
+using System.Reflection;
+using System.Text;
+
+namespace Api.Host.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+             services.AddCors(options =>
+             {
+                 options.AddPolicy("CorsPolicy", builder =>
+                 builder.AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .WithExposedHeaders("X-Pagination")
+                 .AllowAnyHeader());
+             });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+         services.Configure<IISOptions>(options =>
+         {
+
+         });
+
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+                services.AddSingleton<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+                services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+                services.AddScoped<IServiceLocator, ServiceLocator>();
+
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(
+                opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
+                opt.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                opt.ApiVersionReader = new QueryStringApiVersionReader("api-version");
+            });
+        }
+        public static void ConfigureResponseCaching(this IServiceCollection services) =>
+                services.AddResponseCaching();
+
+        public static void ConfigureHttpCacheHeaders(this IServiceCollection services) =>
+             services.AddHttpCacheHeaders((expirationOpt) =>
+             {
+                 expirationOpt.MaxAge = 65;
+                 expirationOpt.CacheLocation = CacheLocation.Private;
+             },
+             (validationOpt) =>
+             {
+                 validationOpt.MustRevalidate = true;
+             });
+        public static void ConfigureRateLimitingOptions(this IServiceCollection services)
+        {
+            var rateLimitRules = new List<RateLimitRule>
+            {
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Limit = 3,
+                    Period = "5m"
+                 }
+             };
+             services.Configure<IpRateLimitOptions>(opt => {
+                     opt.GeneralRules = rateLimitRules;
+             });
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<User, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = true;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 10;
+                o.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<RepositoryContext>()
+            .AddDefaultTokenProviders();
+        }
+
+
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+
+        {
+
+            var jwtConfiguration = new JwtConfiguration();
+            configuration.Bind(jwtConfiguration.Section, jwtConfiguration);
+
+            var jwtSettings = configuration?.GetSection("JwtSettings");
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtConfiguration.ValidIssuer,
+                    ValidAudience = jwtConfiguration.ValidAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["secretKey"]))
+                };
+            });
+        }
+
+        public static void AddJwtConfiguration(this IServiceCollection services,
+            IConfiguration configuration) =>
+                    services.Configure<JwtConfiguration>(configuration.GetSection("JwtSettings"));
+
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Khaled Soltan Api",
+                    Version = "v1",
+                    Description = "CompanyEmployees API by CodeMaze",
+                    TermsOfService = new Uri("https://www.linkedin.com/in/khaled-soltan/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Khaled soltan",
+                        Email = "khaled.soltan.taha@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/khaled-soltan/"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "CompanyEmployees API LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+
+
+                });
+                //s.SwaggerDoc("v2", new OpenApiInfo
+                //{
+                //    Title = "Khaled Soltan Api",
+                //    Version = "v2"
+                //});
+                var xmlFile = $"{typeof(Presentation.AssemblyReference).Assembly.GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Place to add JWT with Bearer",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                          Reference = new OpenApiReference
+                          {
+                             Type = ReferenceType.SecurityScheme,
+                             Id = "Bearer"
+                          },
+                             Name = "Bearer",
+                          },
+                            new List<string>()
+                           }
+                    });
+                });
+
+         
+
+
+        }
+    }
+}
---
+++ Api.Host/Program.cs
@@ -0,0 +1,97 @@
+
+using Api.Host.Extensions;
+using AspNetCoreRateLimit;
+using Business.GenericServices.Contracts;
+using Business.GenericServices.Implmentation;
+using Domain.ErrorModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NLog;
+using Presentation.ActionFilters;
+using Presentation.Controllers;
+using Sciensoft.Hateoas.Extensions;
+using Shared.LoggerService;
+using System.Reflection;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+    config.InputFormatters.Insert(0, GetJsonPatchInputFormatter());
+    config.CacheProfiles.Add("120SecondsDuration", new CacheProfile
+    {
+        Duration = 120
+
+    });
+}).AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
+builder.Services.ConfigureHttpCacheHeaders();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(typeof(Business.AssemblyReference).Assembly);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureVersioning();
+builder.Services.ConfigureRateLimitingOptions();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ValidationFilterAttribute>();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+builder.Services.AddAuthentication();
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureResponseCaching();
+builder.Services.AddMemoryCache();
+builder.Services.ConfigureJWT(builder.Configuration);
+builder.Services.AddJwtConfiguration(builder.Configuration);
+builder.Services.ConfigureSwagger();
+
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+if (app.Environment.IsProduction())
+    app.UseHsts();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "Khaled Soltan API v1");
+    s.SwaggerEndpoint("/swagger/v2/swagger.json", "Khaled Soltan v2");
+});
+
+app.UseHttpsRedirection();
+app.UseResponseCaching();
+app.UseAuthorization();
+app.UseHttpCacheHeaders();
+app.MapControllers();
+app.UseAuthentication();
+app.UseIpRateLimiting();
+app.Run();
+NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter() =>
+    new ServiceCollection().AddLogging().AddMvc().AddNewtonsoftJson()
+        .Services.BuildServiceProvider()
+        .GetRequiredService<IOptions<MvcOptions>>().Value.InputFormatters
+        .OfType<NewtonsoftJsonPatchInputFormatter>().First();
